a11694d18b8af62b95ec5c315da77ef1403857e7
jeremy lorette <jeremy.lorette@outlook.com>
2022-03-03 11:11:07 +0100

    cont URL in function

--- weatherReport/code.js
+++ weatherReport/code.js
@@ -1,34 +1,35 @@
-let ville = "feytiat"
-
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=5ec3b21672a8b8f0636dd017dae65dbd&units=metric";
-
-function recevoirTemperature(newVille){
-ville = newVille
-let requete = new XMLHttpRequest();
-requete.open("GET", url);
-requete.responseType = "json";
-requete.send();
-
-requete.onload = function () {
-  if (requete.readyState === XMLHttpRequest.DONE) {
-    if (requete.status === 200) {
-      let reponse = requete.response;
-      let température = reponse.main.temp;
-      let name = reponse.name;
-      document.querySelector("#ville").textContent = name;
-      document.querySelector("#temperature_label").textContent = température;
-    } else {
-      alert("Un problème est survenu merci de revenir plus tard !");
-    }
-  }
-}};
-
-
+let ville = "paris";
+recevoirTemperature(ville);
 
 let btn = document.querySelector("#changer");
 
-btn.addEventListener("click" , () => {
-     ville = prompt("Quelle ville ?");
-    recevoirTemperature(ville);
-});
+function recevoirTemperature(ville) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    ville +
+    "&appid=5ec3b21672a8b8f0636dd017dae65dbd&units=metric";
 
+  let requete = new XMLHttpRequest();
+  requete.open("GET", url);
+  requete.responseType = "json";
+  requete.send();
+
+  requete.onload = function () {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response;
+        let température = reponse.main.temp;
+        let ville = reponse.name;
+        document.querySelector("#ville").textContent = ville;
+        document.querySelector("#temperature_label").textContent = température;
+      } else {
+        alert("Un problème est survenu merci de revenir plus tard !");
+      }
+    }
+  };
+}
+
+btn.addEventListener("click", () => {
+  ville = prompt("Quelle ville ?");
+  recevoirTemperature(ville);
+});
