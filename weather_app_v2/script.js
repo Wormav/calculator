bb77bd558bd8c5f1c180109b865e16049754ecca
jeremy lorette <jeremy.lorette@outlook.com>
2022-03-10 13:34:56 +0100

    app weather app v2 ended

--- weather_app_v2/script.js
+++ weather_app_v2/script.js
@@ -1,5 +1,52 @@
-let villeChoisie = "saint-saulve";
-recevoirTemperature(villeChoisie);
+let villeChoisie;
+
+// géolocalisation
+
+if("geolocation" in navigator){
+  navigator.geolocation.watchPosition((position) => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+
+    let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+    requete.open('GET', url); // Nous récupérons juste des données
+    requete.responseType = 'json'; // Nous attendons du JSON
+    requete.send(); // Nous envoyons notre requête
+  
+    // Dès qu'on reçoit une réponse, cette fonction est executée
+    requete.onload = function() {
+      if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+          let reponse = requete.response;
+          // console.log(reponse);
+          let temperature = reponse.main.temp;
+          let ville       = reponse.name;
+          // console.log(temperature);
+          document.querySelector('#temperature_label').textContent = temperature;
+          document.querySelector('#ville').textContent = ville;
+        }
+        else {
+          alert('Un problème est intervenu, merci de revenir plus tard.');
+        }
+      }
+    }
+  }, erreur , options);  // erreur fonction erreur et options
+}
+else {
+  villeChoisie = 'paris';
+  recevoirTemperature(villeChoisie);
+}  
+
+var options = {
+  enableHighAccuracy : true ,
+} // améiliore la localisation
+
+function erreur() {
+  villeChoisie = 'paris';
+  recevoirTemperature(villeChoisie);
+}
+;
+// bouton changer de ville
+
+
 
 let changerDeVille = document.querySelector('#changer');
 changerDeVille.addEventListener('click', () => {
